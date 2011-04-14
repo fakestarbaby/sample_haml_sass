489d32c50cacf5a819c41a0e2d618e05a2a73c77
fakestarbaby <fakestarbaby@gmail.com>
2011-04-14 09:10:47 +0900

    初回コミット。

---
+++ Gemfile
@@ -0,0 +1,15 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.6'
+
+# Bundle the extra gems:
+gem 'haml-rails'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+# Bundle gems for the local environment. Make sure to
+# put test-only gems in this group so their generators
+# and rake tasks are available in development mode:
+group :development do
+  gem 'hpricot'
+  gem 'ruby_parser'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleHamlSass::Application.config.secret_token = 'ddcaced3f5aa6a4807139b6a98f735e7091fa5e22bc34bf6e2bfcc9fdb8e65d39f1a8546323dcf9de3498e2c4fd19ef220f18418f0df5916e4eee50492533bf5'
